fbde788a132e74a8c6e6591ff2036e5f4def68dc
Luke Tang <tanlu824@student.otago.ac.nz>
2023-03-07 10:52:05 +1300

    Center webpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ME2FMC';
+
+  squadmateColumns = ['name', 'recruited', 'loyal'];
+  squadmateStatusData = [
+    { name: 'Zaeed', recruited: true, loyal: true },
+    { name: 'Legion', recruited: true, loyal: true },
+    { name: 'Samara', recruited: true, loyal: true },
+    { name: 'Tali', recruited: true, loyal: true },
+    { name: 'Mordin', recruited: true, loyal: true },
+    { name: 'Garrus', recruited: true, loyal: true },
+    { name: 'Miranda', recruited: true, loyal: true },
+    { name: 'Grunt', recruited: true, loyal: true },
+    { name: 'Jacob', recruited: true, loyal: true },
+    { name: 'Thane', recruited: true, loyal: true },
+    { name: 'Jack', recruited: true, loyal: true },
+    { name: 'Kasumi', recruited: true, loyal: true }
+  ];
+
+  shipUpgradeColumns = ['name', 'included'];
+  shipUpgradeData = [
+    { name: 'Heavy Ship Armor', included: false },
+    { name: 'Multicore Shielding', included: false },
+    { name: 'Thanix Cannon', included: false }
+  ];
+
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+  ventSpecialists = [
+    {value: 'tali', viewValue: 'Tali'},
+    {value: 'mordin', viewValue: 'Mordin'},
+    {value: 'thane', viewValue: 'Thane'},
+    {value: 'kasumi', viewValue: 'Kasumi'},
+    {value: 'garrus', viewValue: 'Garrus'},
+    {value: 'jacob', viewValue: 'Jacob'},
+    {value: 'legion', viewValue: 'Legion'},
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
